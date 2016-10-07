0b42971504930ece30c13853105a6ee428cb2e80
Kathy Walrath <kathyw@google.com>
2016-10-06 19:25:54 -0700

    move 1-base into master branch
    from step1 branch

---
+++ 1-base/lib/help/help.dart
@@ -0,0 +1,17 @@
+import 'package:angular2/core.dart';
+import 'package:angular2/src/common/directives.dart';
+
+@Component(
+  selector: 'help-component',
+  templateUrl: 'help.html',
+  styleUrls: const ['help.css'],
+  directives: const [
+    NgSwitch,
+    NgSwitchWhen,
+    NgSwitchDefault
+  ]
+)
+class HelpComponent {
+  @Input()
+  String content;
+}
