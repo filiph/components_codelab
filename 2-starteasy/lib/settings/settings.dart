b9877b4fca5449a131dbdd325540d71bef203a6d
Filip Hracek <filip.hracek@gmail.com>
2016-10-19 19:46:29 +0200

    Fix license headers

--- 2-starteasy/lib/settings/settings.dart
+++ 2-starteasy/lib/settings/settings.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2016, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 import 'package:angular2/core.dart';
 
 import 'package:components_codelab/lottery/lottery.dart';
